cc8f1addddfa6d87e82619b9b53d6c9ba64a5d9f
aamna-sys <aamna.sayyed@systemsltd.com>
2022-10-27 12:31:28 +0500

    Update nav text according to the activated route

--- src/app/containers/default-layout/default-header/default-header.component.ts
+++ src/app/containers/default-layout/default-header/default-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { ClassToggleService, HeaderComponent } from '@coreui/angular';
 
@@ -9,12 +10,23 @@ import { ClassToggleService, HeaderComponent } from '@coreui/angular';
 })
 export class DefaultHeaderComponent extends HeaderComponent {
   @Input() sidebarId: string = 'sidebar';
+  navText: string;
 
   public newMessages = new Array(4);
   public newTasks = new Array(5);
   public newNotifications = new Array(5);
 
-  constructor(private classToggler: ClassToggleService) {
+  constructor(private classToggler: ClassToggleService, private router: Router) {
     super();
+
+    this.router.events.subscribe(() => {
+      if (this.router.url.includes("two")) {
+        this.navText = "International Outbreak"
+      } else if (this.router.url.includes("three")){
+        this.navText = "Epidemic Visualization"
+      } else {
+        this.navText = "Data Generator"
+      }
+    })
   }
 }
