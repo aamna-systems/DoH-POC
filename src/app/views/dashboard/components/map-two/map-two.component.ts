c7b4ae5e26b7fbd996f919da50473df8daf1382b
aamna-sys <aamna.sayyed@systemsltd.com>
2022-11-01 16:21:49 +0500

    Remove commented code

--- src/app/views/dashboard/components/map-two/map-two.component.ts
+++ src/app/views/dashboard/components/map-two/map-two.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import Feature from 'ol/Feature';
 import Map from 'ol/Map';
 import Point from 'ol/geom/Point';
 import View from 'ol/View';
-import { Circle as CircleStyle, Fill, Stroke, Style, Text } from 'ol/style';
+import { Circle as CircleStyle, Fill, Style, Text } from 'ol/style';
 import { Cluster, OSM, Vector as VectorSource } from 'ol/source';
 import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
-import { boundingExtent } from 'ol/extent';
 import * as proj from 'ol/proj';
 import * as layer from 'ol/layer';
 import { Overlay } from 'ol';
@@ -23,9 +22,7 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class MapTwoComponent implements OnInit {
   formValue: FilterFormData;
-  // private formValueSub: Subscription;
   private getSub: Subscription;
-  // private coordinatesSub: Subscription;
   private emirate;
   private coordinates;
 
@@ -37,11 +34,6 @@ export class MapTwoComponent implements OnInit {
   count;
   features;
   e;
-  // for (let i = 0; i < count; ++i) {
-  //   const coordinates = [2 * e * Math.random() - e, 2 * e * Math.random() - e];
-  //   features[i] = new Feature(new Point(coordinates));
-  // }
-
   source;
 
   clusterSource;
@@ -72,13 +64,6 @@ export class MapTwoComponent implements OnInit {
 
         console.log(res);
         console.log('CENTER', this.center);
-        // if (this.resCoordinates?.length) {
-        //   this.mapConfiguration();
-        //   console.log('map');
-        // } else {
-        //   this.emptyMapConfiguration();
-        //   console.log('emptyMap');
-        // }
 
         this.showSuccess(res?.message);
       },
@@ -92,53 +77,11 @@ export class MapTwoComponent implements OnInit {
         this.showError(error?.message);
       }
     );
-
-    // this.formValueSub = this.dataShareService.formValueChanged.subscribe(
-    //   (formValue: FilterFormData) => {
-    //     this.formValue = formValue;
-    //     this.emirate = this?.formValue?.emirate ?? 'dubai';
-    //     this.coordinates = this.dataShareService.getCoordinates(this.emirate);
-    //     this.logEmirateData();
-    //     this.mapConfiguration();
-    //   }
-    // );
-
-    // this.coordinatesSub = this.dataShareService.coordinatesReset.subscribe(() => this.resetCoordinates());
-
-    // this.count = 20000;
-    // this.features = new Array(this.count);
-    // this.e = 4500000;
-    // for (let i = 0; i < this.count; ++i) {
-    //   const coordinates = [
-    //     2 * this.e * Math.random() - this.e,
-    //     2 * this.e * Math.random() - this.e,
-    //   ];
-    //   this.features[i] = new Feature(new Point(coordinates));
-    // }
   }
 
   mapConfiguration(): void {
     document.getElementById('map-two').innerHTML = '';
-    // let randomGeoPoints = this.dataShareService.generateRandomPoints(
-    //   this.coordinates,
-    //   20000,
-    //   10000
-    // );
-
     let randomGeoPoints = this.resCoordinates;
-
-    // console.log("COORD - ", this.coordinates)
-    // this.count = 20000;
-    // this.features = new Array(this.count);
-    // this.e = 4500000;
-    // for (let i = 0; i < this.count; ++i) {
-    //   const coordinates = [
-    //     2 * this.e * Math.random() - this.e,
-    //     2 * this.e * Math.random() - this.e,
-    //   ];
-    //   this.features[i] = new Feature(new Point(coordinates));
-    // }
-
     this.count = randomGeoPoints?.length;
 
     if (this.count) {
@@ -151,7 +94,6 @@ export class MapTwoComponent implements OnInit {
           lat: responseAddress.latitude,
         });
       }
-      console.log('coord - ', co);
 
       this.features = new Array(this.count);
       for (var i = 0; i < this.count; ++i) {
@@ -222,15 +164,11 @@ export class MapTwoComponent implements OnInit {
     var map = new Map({
       layers: [raster, clusters],
       target: 'map-two',
-      // interactions: [],
       view: new View({
-        // center: proj.fromLonLat([144.847275, 13.566806]),
-        // center: proj.fromLonLat([55.2708, 25.2048]),
         center: proj.fromLonLat([this.center.lng, this.center.lat]),
         zoom: 13,
         projection: 'EPSG:3857',
       }),
-      // controls: [],
     });
 
     /* Vector Feature Popup */
@@ -268,37 +206,6 @@ export class MapTwoComponent implements OnInit {
     });
   }
 
-  // emptyMapConfiguration(): void {
-  //   document.getElementById('map-two').innerHTML = '';
-
-  //   const map = new Map({
-  //     layers: [
-  //       new TileLayer({
-  //         source: new OSM(),
-  //       }),
-  //     ],
-  //     target: 'map-two',
-  //     view: new View({
-  //       center: proj.fromLonLat([this.center.lng, this.center.lat]),
-  //       zoom: 11,
-  //     }),
-  //   });
-  // }
-
-  resetCoordinates(): void {
-    this.emirate = 'dubai';
-    this.coordinates = { lat: 25.2048, lng: 55.2708 };
-    console.log('COORDINATES RESET...');
-    this.logEmirateData();
-  }
-
-  logEmirateData(): void {
-    console.log('Component: Map 2');
-    console.log('Emirate:', this.emirate);
-    console.log('Coordinates:', this.coordinates);
-    console.log('----');
-  }
-
   showSuccess(success = 'Data successfully fetched') {
     this.toastr.success(success, 'Success!', {
       timeOut: 5000,
@@ -314,10 +221,6 @@ export class MapTwoComponent implements OnInit {
   }
 
   ngOnDestroy(): void {
-    // if (this.formValueSub) {
-    //   this.formValueSub.unsubscribe();
-    // }
-
     if (this.getSub) {
       this.getSub.unsubscribe();
     }
