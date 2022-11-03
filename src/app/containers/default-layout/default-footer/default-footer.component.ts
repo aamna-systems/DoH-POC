dc2c24b8a0cd3dac28753ef326649dc2b6ad5ddf
aamna-sys <aamna.sayyed@systemsltd.com>
2022-11-03 16:43:37 +0500

    Fix: Legend is shown conditionally

--- src/app/containers/default-layout/default-footer/default-footer.component.ts
+++ src/app/containers/default-layout/default-footer/default-footer.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FooterComponent } from '@coreui/angular';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-default-footer',
   templateUrl: './default-footer.component.html',
   styleUrls: ['./default-footer.component.scss'],
 })
-export class DefaultFooterComponent extends FooterComponent {
-  constructor() {
+export class DefaultFooterComponent
+  extends FooterComponent
+  implements OnInit, OnDestroy
+{
+  private routerSub: Subscription;
+  showLegend: boolean = false;
+
+  constructor(private router: Router) {
     super();
+  }
+
+  ngOnInit(): void {
+    this.routerSub = this.router.events.subscribe(() => {
+      this.showLegend = this.router.url.includes('map') ? true : false;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerSub) {
+      this.routerSub.unsubscribe();
+    }
   }
 }
